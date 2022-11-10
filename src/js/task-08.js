de2c1bf6ebed8df3222b67a39194f53092b8cd73
Alyona-Nekhryshchyniuk <o.nekhryshchyniuk@gmail.com>
2022-11-10 22:44:06 +0200

    8 task done

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector("form");
 
+const valueOnFormSubmit = (event) => {
+  event.preventDefault();
+
+  if (form.password.value.length === 0 || form.email.value.length === 0) {
+    alert("All fields must be filled in!");
+  } else {
+    let obj = {};
+    obj.password = form.elements.password.value;
+    obj.email = form.elements.email.value;
+    console.log(obj);
+
+    form.reset();
+  }
+};
+
+form.addEventListener("submit", valueOnFormSubmit);
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,22 @@
+import { GetColorName } from "hex-color-to-color-name";
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const sources = {
+  body: document.querySelector("body"),
+  btn: document.querySelector("button"),
+  colorName: document.querySelector("span"),
+};
+
+const changeBgcOnClick = () => {
+  // body.style.backgroundColor = getRandomHexColor();
+  // let hex = getRandomHexColor();
+  let result = name("#6195ed");
+  console.log(result[0]);
+  console.log(result[1]);
+  console.log(result[2]);
+  // colorName.textContent = ``;
+};
+
+sources.btn.addEventListener("click", changeBgcOnClick);

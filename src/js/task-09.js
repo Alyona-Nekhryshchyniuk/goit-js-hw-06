1593d7f6ea4f452d39da2a2106b3e9216c6ae255
Alyona-Nekhryshchyniuk <o.nekhryshchyniuk@gmail.com>
2022-11-12 18:55:29 +0200

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,4 +1,3 @@
-import { GetColorName } from "hex-color-to-color-name";
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
@@ -10,13 +9,7 @@ const sources = {
 };
 
 const changeBgcOnClick = () => {
-  // body.style.backgroundColor = getRandomHexColor();
-  // let hex = getRandomHexColor();
-  let result = name("#6195ed");
-  console.log(result[0]);
-  console.log(result[1]);
-  console.log(result[2]);
-  // colorName.textContent = ``;
+  sources.body.style.backgroundColor = getRandomHexColor();
 };
 
 sources.btn.addEventListener("click", changeBgcOnClick);
